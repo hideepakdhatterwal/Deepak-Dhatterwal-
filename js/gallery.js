a071759710e1227699871846a63fe968c8eb9c71
Deepak Dhatterwal <hideepakdhatterwal@gmail.com>
2026-08-14 05:39:28 +0530

    Delete js directory

--- js/gallery.js
+++
@@ -1,9 +0,0 @@
-/* Gallery Image Interactions */
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryImages = document.querySelectorAll('.gallery-card img');
-  galleryImages.forEach(img => {
-    img.addEventListener('click', () => {
-      console.log('Image clicked:', img.src);
-    });
-  });
-});
